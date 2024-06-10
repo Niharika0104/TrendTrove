4ad7435205ddd27b8e522b41360890a110011b65
Mdkaif-123 <amdkaif843@gmail.com>
2024-06-10 21:30:39 +0530

    💄 refactor: Landing Page Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import ReactDOM from "react-dom/client";
-import Login from './Components/Login';
-import Register from './Components/Register';
-import Products from './Components/Products';
-import HomePage from './Components/HomePage';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import NotFound from './Components/NotFound';
-
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+import Products from "./Components/Products";
+import HomePage from "./Components/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFound from "./Components/NotFound";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route exact path="/" element={ <HomePage/>} ></Route>
-      <Route exact path="/login" element={ <Login/>} />
-      <Route path="/sign-up" element={<Register/>}></Route>
-      <Route path="/men" element={<Products/>}></Route>
-      <Route path="/women" element={<Products/>}></Route>
-      <Route path="/kids" element={<Products/>}></Route>
-      <Route path="*" element={<NotFound/>}></Route>
-</Routes>
-</BrowserRouter>
-   
-
- 
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<HomePage />}></Route>
+          <Route exact path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<Register />}></Route>
+          <Route path="/men" element={<Products />}></Route>
+          <Route path="/women" element={<Products />}></Route>
+          <Route path="/kids" element={<Products />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }

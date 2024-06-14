b3c7708c9a5d5a31a03252248f75e41075fb3c49
Rajneesh Kumar <rajneeshkumar2093@gmail.com>
2024-06-14 11:18:49 +0530

    improved the search functionality in search component

--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -5,31 +5,30 @@ import data from '../Assests/HomePageData';
 import CategoryCard from './CategoryCard';
 import Icons from './Icons';
 import CarouselComponent from './CarouselComponent';
+import { useEffect } from 'react';
 
 function HomePage() {
   const navigate = useNavigate();
-  const [query, setQuery] = useState('');
-
-  const handleSearch = (searchQuery) => {
-    setQuery(searchQuery);
-  };
-
-  const filteredData = data.filter((item) =>
-    item.title.toLowerCase().includes(query.toLowerCase())
-  );
+  const [searchedData, setSearchedData] = useState([]);
+   
+  useEffect(() => {
+    console.log(searchedData);
+  }, [searchedData, setSearchedData]);
 
   return (
     <div>
-      <Navbar query={query} onSearch={handleSearch} />
+      <Navbar setSearchedData={setSearchedData} />
       <CarouselComponent />
       <Icons />
       <div>
         <div className="font-extrabold text-center">SHOP BY CATEGORY</div>
         <div className="flex flex-wrap justify-center gap-3 w-5/6 mx-auto">
-          {filteredData.map((item, i) => (
+        {(searchedData.length > 0 ? searchedData : data).map((item, i) => (
             <div
               key={i}
-              onClick={() => navigate("/" + item.search)}
+              onClick={() => {
+                navigate("/" + item.search);
+              }}
               className="cursor-pointer"
             >
               <CategoryCard
